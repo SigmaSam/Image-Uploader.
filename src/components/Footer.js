cc69582a90ff639efea384587f8c8b3f5a6609b5
Samuel Almao Herrera <samuelalmaoherrera@gmail.com>
2022-04-28 00:22:25 -0300

    Finished Initial Scree.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+        <p>Created by <strong>Samuel Almao</strong> - devChallenges.io</p>
+    </div>
   )
 }
 
