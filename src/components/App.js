d827f0fad0aa7346124a36c05e20b0890dc93e16
Samuel Almao Herrera <samuelalmaoherrera@gmail.com>
2022-04-27 23:47:15 -0300

    Initial Layout of the Front-end.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+import '../styles/App.css';
+import Uploader from './Uploader';
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div >
+      <Uploader />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
